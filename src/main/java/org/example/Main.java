305e42eb5b3a5ea9dcd171ba6d1876b9f52241ad
saway <sawa.y918@gmail.com>
2025-02-11 23:04:15 +0900

    fix: 第6回課題 mainメソッド内の、コンソールに出力する際の表記を修正

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ public class Main {
   public static void main(String[] args) {
 
     Kouza07 kouza07 = new Kouza07();
+    System.out.println("課題１");
+    kouza07.kadai1();
+    System.out.println("課題２");
     kouza07.kadai2();
 
   }
